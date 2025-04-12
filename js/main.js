57e8343e33a9e150668b615ca0946196a535fb57
Wangweibing <lpstudio2006@gmail.com>
2025-04-12 19:29:17 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,6 +1,44 @@
 const burger = document?.querySelector('[data-burger]');
 const nav = document?.querySelector('[data-nav]');
+const navItems = nav.querySelectorAll('a');
+const body = document.body;
 
 burger?.addEventListener('click', () => {
-  nav.classList.toggle('nav-visible');
+  body.classList.toggle('stop-scroll');
+  burger?.classList.toggle('burger-active');
+  nav?.classList.toggle('nav-visible');
 });
+
+navItems.forEach((el) => {
+  el.addEventListener('click', () => {
+    body.classList.remove('stop-scroll');
+    burger?.classList.remove('burger-active');
+    nav?.classList.remove('nav-visible');
+  });
+});
+
+// Stiky navigation
+
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add('sticky');
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    // in the viewportr
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  },
+);
+obs.observe(sectionHeroEl);
